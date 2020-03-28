f4550dd26665affe0ae2d2dd6141711d35689d1d
osky9913 <m.osvald9913@gmail.com>
2020-03-28 16:45:24 +0100

    Finished lower part

--- task2/initTablesLowerPart.sql
+++ task2/initTablesLowerPart.sql
@@ -1,15 +1,29 @@
 Create Table "GRIMOIRE"(
-        "ID_Grimoar" CHARACTER VARYING(100) CONSTRAINT "ID_Grimoar_PK" PRIMARY KEY,
+        "ID_GRIMOAR" CHARACTER VARYING(100) CONSTRAINT "ID_Grimoar_PK" PRIMARY KEY,
         "State_of_charged_magic" integer  constraint "State_of_charged_magic_NN" NOT NULL,
         "Name_of_grimoire" CHARACTER VARYING(100) CONSTRAINT "NOT NULL" NOT NULL
     );
-Create Table "MAGICAL BEING"(
+-- Vytvorit id rodne cislo
+Create Table "MAGICAL_BEING"(
+        "ID_MAGICAL_BEING" CHARACTER VARYING(100) CONSTRAINT "ID_Magical_Being_PK" PRIMARY KEY,
+        "Story"CHARACTER VARYING(200)  constraint "MAGICAL_BEING_Story_NN" NOT NULL,
+        "Name" CHARACTER VARYING(100) CONSTRAINT "MAGICAL_BEING_Name_NN" NOT NULL,
+        "Type" CHARACTER VARYING(100) CONSTRAINT "MAGICAL_BEING_Type_NN" CHECK ( "Type"='ELF' OR "Type"='MAGICIAN'),
+        "Mana size" integer CHECK ("Mana size">= 0)  ,
+        "Level" integer CHECK ( "Level" >= 0) ,
+        "Age" integer CHECK ("Age">=0)
+
 
 
 );
-Create Table "SCROLL";
-Create Table "MAGICIAN";
-Create Table "ELF";
+
+-- Vytvorit id isbn
+Create Table "SCROLL" (
+    "ID_SCROLL" NUMBER CONSTRAINT "ID_SCROLL_PK" PRIMARY KEY,
+    "Name" CHARACTER VARYING(100) CONSTRAINT "SCROLL_Name_NN" NOT NULL,
+    "USED" integer CONSTRAINT "SCROLL_USED_NN"  CHECK ( "USED" = 0 OR "USED" = 1)
+
+);
 
 
 
