b906eed73ab90f957cd151e92fa9e9739e234d7d
osky9913 <m.osvald9913@gmail.com>
2020-03-28 15:32:28 +0100

    Grimoire

---
+++ task2/initTablesLowerPart.sql
@@ -0,0 +1,17 @@
+Create Table "GRIMOIRE"(
+        "ID_Grimoar" CHARACTER VARYING(100) CONSTRAINT "ID_Grimoar_PK" PRIMARY KEY,
+        "State_of_charged_magic" integer  constraint "State_of_charged_magic_NN" NOT NULL,
+        "Name_of_grimoire" CHARACTER VARYING(100) CONSTRAINT "NOT NULL" NOT NULL
+    );
+Create Table "MAGICAL BEING"(
+
+
+);
+Create Table "SCROLL";
+Create Table "MAGICIAN";
+Create Table "ELF";
+
+
+
+
+
