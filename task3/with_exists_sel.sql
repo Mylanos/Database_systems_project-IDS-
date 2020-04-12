d779f058a0fd3314585a27a9286a7d84c91e9b27
osky9913 <m.osvald9913@gmail.com>
2020-04-12 14:46:58 +0200

    exist finished

---
+++ task3/with_exists_sel.sql
@@ -0,0 +1,8 @@
+-- Ak existuju kuzelníci ktorí maju pozitivnu synergiu s elementom vacsiu ako 50 tak vypis bytosti s pozitivnou synergiou vacsou ako 50
+
+Select * From
+(Select * from MAGICAL_BEING where
+exists (
+Select * from POSITIVE_SYNERGY Where "limit" > 50) ) magical_being
+inner join ( Select * from POSITIVE_SYNERGY Where "limit" > 50) positive
+ON  positive.ID_MAGICAL_BEING = magical_being.ID_MAGICAL_BEING
