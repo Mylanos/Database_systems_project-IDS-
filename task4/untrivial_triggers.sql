046ba3fed61eda2e87ea47ae99feefd15e7a6017
osky9913 <m.osvald9913@gmail.com>
2020-04-30 22:25:25 +0200

    END

--- task4/untrivial_triggers.sql
+++ task4/untrivial_triggers.sql
@@ -1,3 +1,18 @@
 -- vytvoření alespoň dvou netriviálních databázových triggerů vč. jejich předvedení, z toho právě jeden trigger pro
 -- automatické generování hotnot primárního klíče nějaké tabulky ze sekvence (např. pokud bude při vkládání záznamů
 -- do dané tabulky hodnota primárního klíče nedefinována, tj. NULL),
+
+
+
+CREATE OR REPLACE TRIGGER GRIMOIRE_TRIGGER BEFORE
+INSERT ON GRIMOIRE
+FOR EACH ROW
+WHEN ( NEW."idElement" IS NULL  )
+BEGIN
+   :NEW."idElement" := 'elem01918';
+END;
+
+
+
+INSERT INTO GRIMOIRE (ID_GRIMOAR,"Name_of_grimoire","State_of_charged_magic")
+VALUES ( SYS_GUID(),'hello world' ,1);
